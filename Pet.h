f2c060d5299c50f779ba1df72780cd7e97dd9126
oliveira._.j <juliamatosdeoliveira51@gmail.com>
2026-06-12 18:59:38 -0300

    Update Pet.h

--- Pet.h
+++ Pet.h
@@ -2,21 +2,32 @@
 #define PET_H
 
 #include <string>
+#include <iostream>
 
 class Pet {
-private:
-    std::string nome;
-    int idade;
-    std::string porte;
-    bool possuiDeficiencia;
-    bool castrado;
+    private:
+        int id;
+        std::string nome;
+        std::string tipo; // qual seria o animal
+        int idade;
+        std::string porte;
+        bool possuiDeficiencia;
+        bool castrado;
 
-public:
-    Pet(); // construtor padrão
+    public:
+        Pet(int id, std::string nome, std::string tipo, int idade, std::string porte, bool possuiDeficiencia, bool castrado); // construtor padrão
 
+        std::string getNome() const;
+        std::string getTipo() const;
+        int getIdade() const;
+        std::string getPorte() const;
+        bool getPossuiDeficiencia() const;
+        bool getCastrado() const;
+
+    void editar(std::string nome, std::string porte, int idade);
     void cadastrarPet();   // pergunta os dados ao usuário
     void exibirPet();      // mostra os dados cadastrados
 };
 
 
-#endif
+#endif 
