b7331b95f6b9a7f6019d802cff9dad3bb7c1292d
oliveira._.j <juliamatosdeoliveira51@gmail.com>
2026-06-16 22:24:36 -0300

    Create adotanteFactory.h

---
+++ adotanteFactory.h
@@ -0,0 +1,14 @@
+#ifndef ADOTANTEFACTORY_H
+#define ADOTANTEFACTORY_H
+#include "adotante.h"
+
+
+    class AdotanteFactory {
+        public:
+            static Adotante criarAdotante(int id);
+    };
+
+
+
+
+#endif 
