b5fab93e15fe55210a558b9b662c51ce42750bda
oliveira._.j <juliamatosdeoliveira51@gmail.com>
2026-06-16 22:24:48 -0300

    Create PetFactory.h

---
+++ PetFactory.h
@@ -0,0 +1,23 @@
+#ifndef PETFACTORY
+#define PETFACTORY
+#include "Pet.h"
+
+
+    class PetFactory {
+        public:
+            static Pet criarPet(int id);
+    };
+    
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
