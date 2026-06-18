b2458d9a74e12d20f3e7a0121fc981ab46eb7760
oliveira._.j <juliamatosdeoliveira51@gmail.com>
2026-06-18 17:56:24 -0300

    Update Pet.h

--- Pet.h
+++ Pet.h
@@ -4,29 +4,47 @@
 #include <string>
 #include <iostream>
 
-class Pet {
-    private:
-        int id;
-        std::string nome;
-        std::string tipo; // qual seria o animal
-        int idade;
-        std::string porte;
-        bool possuiDeficiencia;
-        bool castrado;
+    class Pet {
+        private:
+            int id;
+            std::string nome;
+            std::string tipo; // qual seria o animal
+            int idade;
+            std::string porte;
+            bool possuiDeficiencia;
+            bool castrado;
+            bool gostaDePatio;
+            bool adaptaAmbienteFechado;
+            bool sociavelComAnimais;
+            bool sociavelComCriancas;
+            std::string estiloPet;   // saideiro ou caseiro
+            bool carenteAtencao;
 
-    public:
-        Pet(int id, std::string nome, std::string tipo, int idade, std::string porte, bool possuiDeficiencia, bool castrado); // construtor padrão
+        public:
+            Pet();
+            Pet(int id, std::string nome, std::string tipo, int idade, std::string porte,
+            bool possuiDeficiencia, bool castrado,
+            bool gostaDePatio, bool adaptaAmbienteFechado,
+            bool sociavelComAnimais, bool sociavelComCriancas,
+            std::string estiloPet, bool carenteAtencao); // construtor padrão
 
-        std::string getNome() const;
-        std::string getTipo() const;
-        int getIdade() const;
-        std::string getPorte() const;
-        bool getPossuiDeficiencia() const;
-        bool getCastrado() const;
+            int getId() const { return id; }
+            std::string getNome() const { return nome; }
+            std::string getTipo() const { return tipo; }
+            int getIdade() const { return idade; }
+            std::string getPorte() const { return porte; }
+            bool getPossuiDeficiencia() const { return possuiDeficiencia; }
+            bool getCastrado() const { return castrado; }
+            bool getGostaDePatio() const { return gostaDePatio; }
+            bool getAdaptaAmbienteFechado() const { return adaptaAmbienteFechado; }
+            bool getSociavelComAnimais() const { return sociavelComAnimais; }
+            bool getSociavelComCriancas() const { return sociavelComCriancas; }
+            std::string getEstiloPet() const { return estiloPet; }
+            bool getCarenteAtencao() const { return carenteAtencao; }
 
-    void editar(std::string nome, std::string porte, int idade);
-    void cadastrarPet();   // pergunta os dados ao usuário
-    void exibirPet();      // mostra os dados cadastrados
+        void editar(std::string nome, std::string porte, int idade);
+        void cadastrarPet();   // pergunta os dados ao usuário
+        void exibirPet();      // mostra os dados cadastrados
 };
 
 
