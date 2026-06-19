9756e0dc2c51c104ab03929fc18ad482d907e4f7
oliveira._.j <juliamatosdeoliveira51@gmail.com>
2026-06-18 23:03:05 -0300

    Update SistemaAdocaoFacade.h

--- SistemaAdocaoFacade.h
+++ SistemaAdocaoFacade.h
@@ -4,23 +4,31 @@
 #include "RepositorioPets.h"
 #include "RepositorioAdotantes.h"
 
-class SistemaAdocaoFacade {
-private:
-    RepositorioPets repoPets;
-    RepositorioAdotantes repoAdotantes;
+    class SistemaAdocaoFacade {
+        private:
+            RepositorioPets repoPets;
+            RepositorioAdotantes repoAdotantes;
 
-public:
-    SistemaAdocaoFacade();
+        public:
+            SistemaAdocaoFacade();
 
-    // Métodos para Pets
+            // Métodos para Pets
     void cadastrarPet();
     void listarPets();
-    Pet buscarPetPorId(int id);
+        Pet buscarPetPorId(int id);
 
     // Métodos para Adotantes
     void cadastrarAdotante();
     void listarAdotantes();
     Adotante buscarAdotantePorId(int id);
+
+    // Match e relatório
+        int realizarMatch(int idAdotante, int idPet);
+        
+    void realizarMatchAutomatico(int idAdotante);
+    void relatorioPetsDisponiveis();
+    void removerPet(int idPet);
+
 };
 
 #endif
