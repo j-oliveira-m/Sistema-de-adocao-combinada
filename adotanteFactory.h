bbb06f64ffa53731a69bdef6c5d89dd51c7568d4
oliveira._.j <juliamatosdeoliveira51@gmail.com>
2026-06-19 16:54:31 -0300

    Update adotanteFactory.h

--- adotanteFactory.h
+++ adotanteFactory.h
@@ -2,8 +2,11 @@
 #define ADOTANTEFACTORY_H
 #include "adotante.h"
 
+    // Classe responsável por criar objetos Adotante
     class AdotanteFactory {
         public:
+            // Método estático: cria um adotante com base no ID
+            // e chama o cadastro para preencher os dados
             static Adotante criarAdotante(int id);
     };
 
